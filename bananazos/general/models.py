d5da6b12bd531b714420231dc954940a24509211
patoba <patobarrero@gmail.com>
2020-08-30 12:43:12 -0500

    Conexion a la base de datos

--- bananazos/general/models.py
+++ bananazos/general/models.py
@@ -7,13 +7,15 @@ from django.forms import ModelForm
 class Trailer(models.Model):
     id = models.IntegerField(auto_created=True, primary_key=True)
     fecha_salida = models.DateField(auto_created=True,auto_now_add=True)
-    vistas = models.PositiveIntegerField(auto_created=True)
+    vistas = models.PositiveIntegerField(auto_created=True, default=0)
+    likes = models.PositiveIntegerField(auto_created=True, default=0)
+    dislikes = models.PositiveIntegerField(auto_created=True, default=0)
 
     titulo = models.CharField(max_length=100)
     link_video = models.CharField(max_length=400)
     link_imagen = models.CharField(max_length=400)
     duracion = models.CharField(max_length=10, default="0:00")
-
+    
     
     class Meta:
         ordering = ('-fecha_salida',)
@@ -26,4 +28,32 @@ class TrailerForm(ModelForm):
         model = Trailer
         fields = ['titulo', 'link_video', 'link_imagen', 'duracion']
 
+class Comentario(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
+    trailer_id = models.ForeignKey(Trailer, on_delete=models.CASCADE)
+    usuario_username = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField(max_length=500, default="")
+    class Meta:
+        ordering = ('-id',)
+    def __str__(self):
+        return str(self.usuario_username) + ": " + str(self.trailer_id)
 
+class Reaccion(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
+    trailer_id = models.ForeignKey(Trailer, on_delete=models.CASCADE)
+    usuario_username = models.ForeignKey(User, on_delete=models.CASCADE)
+    reaccion = models.ImageField()
+    class Meta:
+        ordering = ('-id',)
+    def __str__(self):
+        return str(self.usuario_username) + ": " + str(self.trailer_id)
+
+class Historial(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
+    usuario_username = models.ForeignKey(User, on_delete=models.CASCADE)
+    trailer_id = models.ForeignKey(Trailer, on_delete=models.CASCADE)
+    fecha = models.DateField(auto_created=True,auto_now_add=True)
+    class Meta:
+        ordering = ('-fecha',)
+    def __str__(self):
+        return str(self.usuario_username) + ": " + str(self.trailer_id)
--- bananazos/general/views.py
+++ bananazos/general/views.py
@@ -1,34 +1,47 @@
 from django.shortcuts import render, redirect
-from .models import Trailer, TrailerForm
+from .models import Trailer, TrailerForm, Comentario, Historial
+from random import choice
 
 def trailers_list(request):
+    trailers = Trailer.objects.all()
+    """
     d = [
         1, "Trailazo", "https://lorempixel.com/800/400/nature/4",
         "4:20", 320]
     l = [d] * 3
-    return render(request, 'general/index.html', {"trailers":l})
+    """
+    return render(request, 'general/index.html', {"trailers":trailers})
 
 def video(request, pk=0):
+    trailer = Trailer.objects.get(id=pk)
+    trailer_siguiente = choice(Trailer.objects.all())
+    comentarios = Comentario.objects.all()
+    nuevo = Historial(trailer_id=trailer, usuario_username=request.user)
+    nuevo.save()
     d = {
-        "titulo":"titulo",
-        "link":"//www.youtube.com/embed/Q8TXgCzxEnw?rel=0",
-        "likes":"30",
-        "dislikes":"20",
+        "titulo":trailer.titulo,
+        "link":trailer.link_video,
+        "likes":trailer.likes,
+        "dislikes":trailer.dislikes,
         "descripcion":"asdasd",
-        "video_recomendado_titulo":"siguiente", 
-        "video_recomendado_duracion":"2:00", 
-        "video_recomendado_miniatura":"https://lorempixel.com/800/400/nature/4",
-        "video_recomendado_vistas":"430",
-        "comentarios":[("hola", "mal trailer"),("alejandro", "buen trailer")]
+        "video_recomendado_titulo":trailer_siguiente.titulo, 
+        "video_recomendado_duracion":trailer_siguiente.duracion, 
+        "video_recomendado_miniatura":trailer_siguiente.link_imagen,
+        "video_recomendado_vistas":trailer_siguiente.vistas,
+        "comentarios":comentarios
     }
     return render(request, 'general/video.html', d)
 
 def historial(request):
+    historial = Historial.objects.filter(usuario_username=request.user)
+    trailers = Trailer.objects.filter(id=historial.trailer_id)
+    """
     d = [
         1, "Trailazo", "https://lorempixel.com/800/400/nature/4",
         "4:20", "320"]
     l = [d] * 3
-    return render(request, 'general/historial.html', {"trailers":l})
+    """
+    return render(request, 'general/historial.html', {"trailers":trailers})
 
 def subir_trailer(response):
     if response.method == "POST":
